85a9d6b15954a80e5daefc6d804e3021548aa0ce
AbhinaV <79316824+Abhinav-Vasireddy@users.noreply.github.com>
2022-11-29 12:29:47 +0530

    Add files via upload
    simlpe producer

---
+++ producer.py
@@ -0,0 +1,25 @@
+from flask import Flask, request
+import requests
+app = Flask(__name__)
+
+@app.route("/")
+def send_msg():
+    f = open('leaders.txt')
+    data = f.read()
+    if(data=='100'):
+        port_num = 5000
+    elif(data=='010'):
+        port_num = 5001
+    elif(data=='001'):
+        port_num = 5002
+    msg = input("Enter your message = ")
+    msg_json = {"message":msg}
+    port_num = "http://localhost:" + str(port_num) + "/test" 
+    res = requests.post("http://localhost:5001/test",json=msg_json)
+    return str(res)
+
+if __name__ == "__main__":
+    app.run(host="localhost", port=5003, debug=True)
+
+
+    
